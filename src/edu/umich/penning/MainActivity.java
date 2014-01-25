65543755313010d3f6d1dc2d4edb4960148540c0
Tim Wood <tdubwood@gmail.com>
2014-01-25 16:53:20 -0500

    undo/redo buttons added

--- src/edu/umich/penning/MainActivity.java
+++ src/edu/umich/penning/MainActivity.java
@@ -1,14 +1,18 @@
 package edu.umich.penning;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
 	public static Context context;
 	public static EditText et;
+	private Button undoButton;
+	private Button redoButton;
 	private CollabEditTextListener listener;
 
     @Override
@@ -18,6 +22,26 @@ public class MainActivity extends Activity {
         context = this;
         et = (EditText) findViewById(R.id.collabEditText1);
         et.addTextChangedListener(listener);
+        
+        undoButton = (Button) findViewById(R.id.undo_button);
+        undoButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				listener.undo();	
+			}
+		});
+        
+        redoButton = (Button) findViewById(R.id.redo_button);
+        redoButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				listener.redo();
+			}
+		});
     }
 
     @Override
