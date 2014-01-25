ab539e6cf7753e01bfc588fa876bb3422f40b493
Adam Oxner <adoxner@umich.edu>
2014-01-25 18:21:57 -0500

    Revert "button toast changes"
    This reverts commit a78c0aa1e163fdbd7b04ca3aed8db0c7d22f1877.

--- src/edu/umich/penning/CollabEditTextListener.java
+++ src/edu/umich/penning/CollabEditTextListener.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * @author adoxner, pramodsum, Tim-Wood
@@ -18,6 +20,11 @@ public class CollabEditTextListener implements TextWatcher {
 	public Stack<Event> undoStack = new Stack<Event>();
 	public Stack<Event> redoStack = new Stack<Event>();
 	protected String fullText;
+	private EditText owningContext;
+	
+	public CollabEditTextListener(EditText e){
+		owningContext = e;
+	}
 	
 	/*
 	 * Tracks cursor changes
--- src/edu/umich/penning/MainActivity.java
+++ src/edu/umich/penning/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
